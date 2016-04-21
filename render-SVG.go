0c0469730e25b095e6174a671611a28be1c58622
xn0px90 <xn0px90@users.noreply.github.com>
2016-04-21 16:39:49 -0400

    Create render-SVG.go
    svg example

---
+++ render-SVG.go
@@ -0,0 +1,21 @@
+//usr/bin/env go $0 @0 ; exit
+
+package main
+
+import (
+	"bytes"
+	"github.com/ajstarks/svgo"
+	"github.com/gopherjs/gopherjs/js"
+)
+
+func main() {
+	var output bytes.Buffer
+
+	canvas := svg.New(&output)
+	canvas.Start(250, 250)
+	canvas.Circle(125, 125, 100, "fill:#28262C;stroke:#5BA642;stroke-width:5px")
+	canvas.Text(125, 135, "xn0px90!", "text-anchor:middle;font-size:36px;fill:#EB5633")
+	canvas.End()
+
+	js.Global.Get("document").Get("body").Set("innerHTML", output.String())
+}
