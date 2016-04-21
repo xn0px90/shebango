08436395188388cf1dcd356dc5ccf86dd25d15a5
xn0px90 <xn0px90@users.noreply.github.com>
2016-04-21 15:29:03 -0400

    Create ncat.go

---
+++ ncat.go
@@ -0,0 +1,18 @@
+//open ports for ncat -l 4444
+  
+package main 
+
+import(
+  "fmt"
+  "os"
+  "net"
+  )
+  
+  func ncat(){
+  }
+  
+  
+  func main (){
+  fmt.Prinln("conn port %s", port)
+  fmt.Prinln("conn made")
+  }
