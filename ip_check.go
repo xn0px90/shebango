1ba345dd6a8c46dbafd96d9fef4c27647b2624a7
xn0px90 <xn0px90@gmail.com>
2022-04-11 13:04:16 -0400

    ip_info update

--- ip_check.go
+++ ip_check.go
@@ -1,1 +1,30 @@
-//check ip 
+//usr/bin/env go run $0 $@ ; exit
+package main
+import (
+    "os"
+    "sync"
+    "os/exec"
+    "fmt"
+)
+
+var wg *sync.WaitGroup
+
+func main() {
+    urls := os.Args[1:]
+
+    wg = new(sync.WaitGroup)
+    wg.Add(len(urls))
+
+    for _, url := range urls {
+        go download(url)
+    }
+
+    wg.Wait()
+}
+
+func download(url string) {
+    defer wg.Done()
+    cmd := exec.Command("curl", "-0", url)
+    cmd.Run()
+    fmt.Printf("%v",cmd)
+}
